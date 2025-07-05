4f0dfc4fb530149d0d2a888eda7aa9d081b4a001
YangJiLei <2031291160@qq.com>
2025-07-05 18:30:38 +0800

    Add all project files

---
+++ resize_back_to_original.py
@@ -0,0 +1,35 @@
+import os
+from PIL import Image
+from tqdm import tqdm
+
+def upscale_all_images(
+    input_dir, output_dir, target_size=(1821, 9705)
+):
+    os.makedirs(output_dir, exist_ok=True)
+
+    print(f"🔄 正在将所有图像统一放大到 {target_size} ...")
+    for fname in tqdm(os.listdir(input_dir)):
+        if fname.lower().endswith((".png", ".jpg", ".jpeg")):
+            input_path = os.path.join(input_dir, fname)
+            try:
+                with Image.open(input_path) as img:
+                    resized_img = img.resize(target_size, Image.BICUBIC)
+                    save_path = os.path.join(output_dir, fname)
+                    resized_img.save(save_path)
+                    print(f"✅ 放大并保存: {fname}，新大小: {target_size}")
+            except Exception as e:
+                print(f"⚠️ 处理图像失败 {fname}: {e}")
+
+    print(f"\n🎉 所有图像已放大并保存到: {output_dir}")
+
+if __name__ == "__main__":
+    input_images_dir = "./checkpoints/xray_patch_model/web/images"  # 输入图片路径
+    output_resized_dir = "./datasets/gen"  # 放大后保存路径
+    target_width = 1821
+    target_height = 9705
+
+    upscale_all_images(
+        input_dir=input_images_dir,
+        output_dir=output_resized_dir,
+        target_size=(target_width, target_height)
+    )
---
+++ train_xray.py
@@ -0,0 +1,33 @@
+import subprocess
+
+
+def train():
+    log_path = "train_log.txt"
+
+    with open(log_path, "w") as f:
+        f.write("Starting training...\n")
+
+    with open("train_log.txt", "w") as f:
+        subprocess.run([
+            "python", "-u", "train.py",
+            "--dataroot", "./datasets/xray_gen",
+            "--name", "xray_patch_model",
+            "--model", "cycle_gan",
+            "--preprocess", "resize",
+            "--gpu_ids", "-1",  # 参数 0 是GPU ,参数 -1 是CPU
+            "--display_id", "10",
+            "--display_freq", "5",
+            "--update_html_freq", "10",
+            "--print_freq", "1",
+            "--num_threads", "0",
+            "--batch_size", "1",
+            "--load_size", "768",
+            # "--crop_size", "768",
+            "--n_epochs", "1",
+            "--n_epochs_decay", "1",
+            "--save_epoch_freq", "1"
+        ], stdout=f, stderr=subprocess.STDOUT, check=True)
+
+
+if __name__ == "__main__":
+    train()
